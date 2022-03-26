fe32a105941a4bbea340ef6eb2b54ec2b8ac8064
Jacob <telljacob@gmail.com>
2022-03-26 17:19:18 +0000

    Add welcome function and additional structure

--- run.py
+++ run.py
@@ -1,22 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome():
+    """
+    This is where we introduce the user to the game and let the user choose role
+    """
+    print("CHAMPION! I have been waiting for you!")
+    print("We need help slaying this beast! What role would you like to play?")
 
 
 def tank_game():
+    """
+    This function implements all your abilities as a tank player
+    """
     print("Hello tanks")
 
+
 def healer_game():
+    """
+    This function implements all your abilities as a healer player
+    """
     print("Hello healers")
 
+
 def dps_game():
+    """
+    This function implements all your abilities as a dps player
+    """
     print("Hello dps")
 
+
 def main():
-    player_health = 100
-    boss_health = 100
-    
-    print("CHAMPION! I have been waiting for you, we need help slaying this vile beast! We are here to support you in any way we can. We can adapt to your preferred role, how do you wish to handle this?")
-    
-    role_choice = input(print("Which role would you like to play for this encounter: "))
-    print(f"A {role_choice}, fantastic choice, just what we need!")
+    """
+    This is the main function
+    """
+    # player_health = 100
+    # boss_health = 100
+    role_choice = input(print("Which role would you like to play: "))
+    print("Fantastic choice, just what we need!")
+    if role_choice == "tank":
+        tank_game()
+    elif role_choice == "healer":
+        healer_game()
+    elif role_choice == "dps":
+        dps_game()
+    else:
+        print("Please input a valid role")    
+
+
+main()

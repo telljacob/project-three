44d284bb1877742a1b7a39e4509bccd1ce761885
Jacob <telljacob@gmail.com>
2022-03-26 16:32:35 +0000

    Add initial conversation with user

--- run.py
+++ run.py
@@ -4,12 +4,19 @@
 
 
 def tank_game():
+    print("Hello tanks")
 
 def healer_game():
+    print("Hello healers")
 
 def dps_game():
+    print("Hello dps")
 
 def main():
     player_health = 100
     boss_health = 100
     
+    print("CHAMPION! I have been waiting for you, we need help slaying this vile beast! We are here to support you in any way we can. We can adapt to your preferred role, how do you wish to handle this?")
+    
+    role_choice = input(print("Which role would you like to play for this encounter: "))
+    print(f"A {role_choice}, fantastic choice, just what we need!")

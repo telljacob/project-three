3f9efae064561c38bac8712c153c9a991689a0fc
Jacob <telljacob@gmail.com>
2022-03-26 16:16:11 +0000

    Add core functions, player health and boss health

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def tank_game():
+
+def healer_game():
+
+def dps_game():
+
+def main():
+    player_health = 100
+    boss_health = 100
+    

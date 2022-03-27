69ed9cab8461b93f21a15b90858e61b285805cf7
Jacob <telljacob@gmail.com>
2022-03-27 04:19:15 +0000

    Add improvement to combat functions

--- run.py
+++ run.py
@@ -19,11 +19,10 @@ def tank_info():
     """
     This function implements all your abilities as a tank player
     """
-    response = input("Do you remember how to tank this boss? yes/no\n")
+    response = input("Do you remember how to play? yes/no\n")
     if response == "no":
         print("Press 1 to strike the boss")
         print("Press 2 to use a defensive from heavy hitting abilities")
-        print("Press 3 to hide from the boss's main ability")
     elif response == "yes":
         print("Excellent! Let's start the fight")
     else:
@@ -33,17 +32,16 @@ def tank_info():
 
 def healer_info():
     """
-    This function implements all your abilities as a healer player
+    This function implements all your abilities as a tank player
     """
-    response = input("Do you remember how to heal this boss? yes/no\n")
+    response = input("Do you remember how to heal? yes/no\n")
     if response == "no":
         print("Press 1 to strike the boss")
-        print("Press 2 to heal the tank")
-        print("Press 3 to hide from the boss's main ability")
+        print("Press 2 to heal the tank when they get low on health")
     elif response == "yes":
         print("Excellent! Let's start the fight")
     else:
-        print("Please respond with yes or no")
+        print("Please respond yes or no")
         healer_info()
 
 
@@ -83,28 +81,49 @@ def main():
     role_choice = input("Type 'tank', 'healer' or 'dps' to choose.\n")
     if role_choice == "tank":
         tank_info()
+        print("Champion! Prepare yourself! Don't forget to use your abilities")
+        while boss_health and player_health > 0:
+            print(f"HP: {player_health}")
+            print(f"BOSS HP: {boss_health}")
+            action = int(input("Attack or heal\n"))
+            if action == 1:
+                boss_health = boss_health - player_hit()
+            elif action == 2:
+                player_health = player_health + player_heal()
+            else:
+                print("You messed up, the boss strikes you twice. Ouch!")
+                player_health = player_health - boss_hit()
+            player_health = player_health - boss_hit()
+
+        if boss_health == 0:
+            print("Congratulations! You defeated the boss!")
+        else:
+            input("The boss is laughing, you should try the boss again?\n")
+
     elif role_choice == "healer":
         healer_info()
+        print("Champion! Prepare yourself! Don't forget to use your abilities")
+        while boss_health and player_health > 0:
+            print(f"Tank HP: {player_health}")
+            print(f"BOSS HP: {boss_health}")
+            action = int(input("Attack or heal the tank\n"))
+            if action == 1:
+                boss_health = boss_health - player_hit()
+            elif action == 2:
+                player_health = player_health + player_heal()
+            else:
+                print("You messed up, the boss striked the tank twice. Ouch!")
+                player_health = player_health - boss_hit()
+            player_health = player_health - boss_hit()
+
+        if boss_health == 0:
+            print("Congratulations! You defeated the boss!")
+        else:
+            input("The boss is laughing, you should try the boss again?\n")
+
     else:
         print("Please choose a valid role")
         main()
-
-    print("Champion! Prepare yourself! And don't forget to use your abilities")
-
-    while boss_health and player_health > 0:
-        boss_hit()
-        print(f"HP: {player_health}")
-        print(f"BOSS HP: {boss_health}")
-        action = int(input("Attack or heal"))
-        if action == 1:
-            boss_health = boss_health - player_hit()
-        elif action == 2:
-            player_health = player_health + player_heal()
-
-    if boss_health == 0:
-        input("Congratulations! You defeated the boss!")
-    else:
-        input("The boss is laughing, would you like to try the boss again?")
 
 
 welcome()
